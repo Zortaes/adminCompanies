c9c49b525177e0571f647f36368253b9312e2838
tiphaine <surygalatiphaine@googlemail.com>
2020-10-26 13:19:41 +0000

    remove possibility to register + need auth to perform companies crud

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('companies', 'App\Http\Controllers\CompaniesController');
+Route::resource('companies', 'App\Http\Controllers\CompaniesController')->middleware('auth');
 
 Auth::routes();
 
